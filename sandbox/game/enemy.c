b7c44327e58e9e54493b7c1d1b6d99542c834bb6
uberdever <diablothird@mail.ru>
2020-09-17 22:11:33 +0300

    Migrated to vs code. Added proper circle x rect collision

---
+++ sandbox/game/enemy.c
@@ -0,0 +1,4 @@
+//
+// Created by uberdever on 09.09.2020.
+//
+
---
+++ sandbox/game/enemy.h
@@ -0,0 +1,8 @@
+//
+// Created by uberdever on 09.09.2020.
+//
+
+#ifndef IND3D_ENEMY_H
+#define IND3D_ENEMY_H
+
+#endif //IND3D_ENEMY_H
